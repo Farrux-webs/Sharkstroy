99552b2c14f6e55b5e9b8397574283280083203e
Farrux-webs <ravshanbekovf02@gmail.com>
2025-06-29 14:44:50 +0500

    fix: gallery part fixed

--- src/components/gallery/gallery.jsx
+++ src/components/gallery/gallery.jsx
@@ -5,6 +5,8 @@ import galleryPhoto3 from "../../assets/images/IMG_8286.JPG";
 import galleryPhoto4 from "../../assets/images/IMG_8292.JPG";
 import galleryPhoto5 from "../../assets/images/IMG_8288.JPG";
 import galleryPhoto6 from "../../assets/images/IMG_8290.JPG";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleRight, faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 
 const Galleries = [
   galleryPhoto1,
@@ -72,21 +74,7 @@ const Gallery = () => {
           data-carousel-prev
         >
           <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg
-              class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 6 10"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M5 1 1 5l4 4"
-              />
-            </svg>
+            <FontAwesomeIcon icon={faAngleLeft} />
             <span class="sr-only">Previous</span>
           </span>
         </button>
@@ -96,21 +84,7 @@ const Gallery = () => {
           data-carousel-next
         >
           <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg
-              class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 6 10"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="m1 9 4-4-4-4"
-              />
-            </svg>
+            <FontAwesomeIcon icon={faAngleRight} />
             <span class="sr-only">Next</span>
           </span>
         </button>
